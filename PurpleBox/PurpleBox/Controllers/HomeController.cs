fb186af211ac3e454d2f6d99a180ab75e57c5db0
Jasmichele <jasmichele@yahoo.com>
2016-10-31 16:30:52 -0400

    somthing

---
+++ PurpleBox/PurpleBox/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PurpleBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult OverDue(int? id)
+        {
+        MovieRentalEntities1 db = new MovieRentalEntities1();
+            List<string> overDue = new List<string>();
+
+            var allMovies = from m in db.RentalRecords
+                            where m.DueDate < DateTime.Today
+                            select m;
+
+
+            return View();
+        }
+    }
+}
---
+++ PurpleBox/PurpleBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PurpleBox.MvcApplication" Language="C#" %>
